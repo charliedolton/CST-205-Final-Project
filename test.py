96f86ef0766ea1073ea30194ecab96234966626e
BarbaraKondo <bkondo@csumb.edu>
2021-11-16 17:56:19 -0800

    made crude mapping system bewteen users & lists.

--- test.py
+++ test.py
@@ -2,12 +2,26 @@ import pandas as pd
 
 class User:
     ids = 0
+    user_id_map = {
+        "0000": "test-user"
+    }
 
     def __init__(self, username, password):
         self.username = username
         self.password = password
-        User.ids += 1
-        self.id = User.ids
+        if (username == "admin"):
+            self.id = 0
+        else:
+            User.ids += 1
+            self.id = User.ids
+        User.user_id_map[str(self.id)] = self.username
+    
+    def get_id(self):
+        return self.id
+
+    def get_str_id(self):
+        return str(self.id)
+
 
 class Movie:
     ids = 0
@@ -21,17 +35,33 @@ class Movie:
     def print_info(self):
         print(f'   title: {m.title}\n   release year: {m.release_year}\n   description: {m.description}')
 
-
+admin = User("admin", "admin")
 user1 = User("user1", "pass1")
 user2 = User("user2", "pass2")
 
+movie0 = Movie("Title0", "0000", "Description0")
 movie1 = Movie("Title1", "1111", "Description1")
 movie2 = Movie("Title2", "2222", "Description2")
 
-favorites = []
-favorites.append(dict({str(user1.id): [movie1, movie2]}))
-print(favorites)
-print("user1's favorites:")
-for m in favorites[0][str(user1.id)]:
-    print("-----")
-    m.print_info()
+# favorites = []
+# favorites.append(dict({str(user1.id): [movie1, movie2]}))
+# print(favorites)
+# print("----------")
+# print("user1's favorites:")
+# for m in favorites[0][str(user1.id)]:
+#     print("-----")
+#     m.print_info()
+
+user_favorites = {
+    "0": [movie0]
+}
+
+user_favorites[user1.get_str_id()] = [movie1, movie2]
+user_favorites[user2.get_str_id()] = [movie2]
+# see test-output.txt for current output :D
+for id in user_favorites:
+    print("----------")
+    print(f"{User.user_id_map[id]}'s favorites:")
+    for m in user_favorites[id]:
+        print("-----")
+        m.print_info()
