fb88c582d8e0013a4102de376d0f9056900bad1d
BarbaraKondo <bkondo@csumb.edu>
2021-12-06 16:51:20 -0800

    - added icon_url to User.py   - should make a page where user can choose icon from givens

--- User.py
+++ User.py
@@ -1,8 +1,8 @@
 class User:
     ids = 0
     # id_to_user_map = {
-    #     # user_id: [username, password]
-    #     "0000": ["test-user", "test-pass"]
+    #     # user_id: [username, email, password, icon_url]
+    #     "0000": ["test-user", "testuser@email.com", "test-pass", "https://www.pexels.com/photo/food-wood-people-school-3843284/ "]
     # }
     # user_to_id_map = {
     #     # username: user_id
@@ -15,15 +15,17 @@ class User:
     current_user_id = None
     is_authenticated = False
 
-    def __init__(self, username, password):
+    def __init__(self, username, email, password, icon_url):
         self.username = username
+        self.email = email
         self.password = password
+        self.icon_url = icon_url
         if (username == 'admin'):
             self.id = 0
         else:
             User.ids += 1
             self.id = User.ids
-        User.id_to_user_map[str(self.id)] = self.username
+        User.id_to_user_map[str(self.id)] = [self.username, self.email, self.password, self.icon_url]
         User.user_to_id_map[self.username] = self.id
     
     def get_id(self):
@@ -35,6 +37,9 @@ class User:
     def get_username(self):
         return self.username
 
+    def get_email(self):
+        return self.email
+
     def get_password(self):
         return self.password
 
@@ -45,5 +50,5 @@ class User:
         return User.id_to_user_map
 
     def add_user(newUser):
-        User.id_to_user_map[newUser.get_str_id()] = [newUser.get_username, newUser.get_password]
-        User.user_to_id_map[newUser.get_username()] = newUser.get_id
+        User.id_to_user_map[newUser.get_str_id()] = [newUser.get_username(), newUser.get_email(), newUser.get_password()]
+        User.user_to_id_map[newUser.get_username()] = newUser.get_id()
