d3f05dd23c8c4f53d525d101291fa9176910fc3c
Barbara Kondo <bkondo@csumb.edu>
2021-12-15 18:33:24 -0800

    adjusted movie.py to match user.py

--- movie.py
+++ movie.py
@@ -3,34 +3,32 @@ from database import Database
 class MovieObj:
 
     def __init__(self, m_id, m_title, m_release_date, m_overview, m_img_url):
-        self.info = dict(
-            movie_id = m_id,
-            title = m_title,
-            release_date = m_release_date,
-            overview = m_overview,
-            img_url = m_img_url
-        )
+            self.movie_id = m_id,
+            self.title = m_title,
+            self.release_date = m_release_date,
+            self.overview = m_overview,
+            self.img_url = m_img_url
 
     def get_id(self):
-        return self.info["movie_id"]
+        return self.movie_id
 
     def get_str_id(self):
-        return str(self.info["movie_id"])
+        return str(self.movie_id)
 
     def get_title(self):
-        return self.info["title"]
+        return self.title
 
     def get_release_date(self):
-        return self.info["release_date"]
+        return self.release_date
 
     def get_overview(self):
-        return self.info["overview"]
+        return self.overview
 
     def get_img_url(self):
-        return self.info["img_url"]
+        return self.img_url
     
     def get_info(self):
         return self.info
 
     def print_info(self):
-        print(f'   id: {self.info["movie_id"]}\n    title: {self.info["title"]}\n   release year: {self.info["release_date"]}\n   overview: {self.info["overview"]}')
+        print(f'   id: {self.movie_id}\n    title: {self.title}\n   release year: {self.release_date}\n   overview: {self.overview}')
